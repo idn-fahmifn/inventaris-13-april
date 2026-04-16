6ab9735b7819454cd5af19d76fb508d0d68e8b43
idn-fahmifn <fahmifn.idn@gmail.com>
2026-04-16 10:54:16 +0700

    4.2

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        // Restriced access for admin and petugas
+
+        if(!Auth::user()->check || !Auth::user()->is_admin) {
+            abort(403, 'Anda tidak bisa akses halaman ini');
+        }
+
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// grup khusus area admin
+Route::prefix('admin')->middleware(['auth', 'verified', 'role'])->group(function () {
+
+    // dashboar admin
+    Route::get('/dashboard', [DashboardController::class, 'admin'])->name('dashboard');
+    
+});
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
