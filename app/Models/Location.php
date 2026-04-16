8c9506fd32f17e47c2f527d7026ffe091d2e9acf
idn-fahmifn <fahmifn.idn@gmail.com>
2026-04-16 13:45:42 +0700

    4.10

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    protected $guarded = [];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    // relasi ke user (petugas)
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class, 'item_id');
+    }
+
 }
