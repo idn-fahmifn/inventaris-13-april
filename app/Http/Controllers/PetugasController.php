e8905222c2acb68873de8c33c026aa5ace3a1774
idn-fahmifn <fahmifn.idn@gmail.com>
2026-04-16 13:25:10 +0700

    4.8

--- app/Http/Controllers/PetugasController.php
+++ app/Http/Controllers/PetugasController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class PetugasController extends Controller
 {
     public function index()
     {
         return view('petugas.index');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_petugas' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+        ]);
+
+        User::create([
+            'name' => $request->nama_petugas,
+            'email' => $request->email,
+            'password' => Hash::make('password'),
+        ]);
+
+        return redirect()->route('petugas.index')->with('success', 'Petugas berhasil ditambahkan.');
+
     }
 }
